873534cc5f0c148e62a997a0cebc0da1190bea31
kristianrafuse <kristianrafuse@gmail.com>
2023-10-12 09:17:35 -0300

    Update

---
+++ ETL/database_setup.sql
@@ -0,0 +1,43 @@
+CREATE TABLE "clearance" (
+	"product_name" varchar,
+	"original_price" float,
+	"clearance_price" float,
+	"percentage_off" float,
+	"product_category" varchar,
+    "product_code" varchar,
+    "rating" float,
+	"link" varchar
+);
+
+CREATE TABLE "hot_sale" (
+	"product_name" varchar,
+	"original_price" float,
+	"clearance_price" float,
+	"percentage_off" float,
+	"product_category" varchar,
+    "product_code" varchar,
+    "rating" float,
+	"link" varchar
+);
+
+CREATE TABLE "red_alert_deals" (
+	"product_name" varchar,
+	"original_price" float,
+	"clearance_price" float,
+	"percentage_off" float,
+	"product_category" varchar,
+    "product_code" varchar,
+    "rating" float,
+	"link" varchar
+);
+
+CREATE TABLE "sales" (
+	"product_name" varchar,
+	"original_price" float,
+	"clearance_price" float,
+	"percentage_off" float,
+	"product_category" varchar,
+    "product_code" varchar,
+    "rating" float,
+	"link" varchar
+);
